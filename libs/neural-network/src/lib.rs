e2525492e340d01ac96e5e8c778d0576de61af8d
Ryland Edwards <rylandcedwards@gmail.com>
2024-08-25 11:16:17 -0500

    add structs and impl in lib.rs for neural net

---
+++ libs/neural-network/src/lib.rs
@@ -0,0 +1,58 @@
+#[derive(Debug)]
+pub struct LayerTopology {
+    pub neurons: usize,
+}
+
+
+#[derive(Debug)]
+pub struct Network {
+    layers: Vec<Layer>,
+}
+
+impl Network {
+    pub fn random(layers: Vec<LayerTopology>) -> Self {
+        todo!("Implement random network generation")
+    }
+
+    pub fn propagate(&self, mut inputs: Vec<f32>) -> Vec<f32> {
+        self.layers
+            .iter()
+            .fold(inputs, |inputs, layer| layer.propagate(inputs))
+    }
+}
+
+
+#[derive(Debug)]
+struct Layer {
+    neurons: Vec<Neuron>,
+}
+
+impl Layer {
+    fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        self.neurons
+            .iter()
+            .map(|neuron| neuron.propagate(&inputs))
+            .collect()
+    }
+}
+
+
+#[derive(Debug)]
+struct Neuron {
+    bias: f32,
+    weights: Vec<f32>,
+}
+
+impl Neuron {
+    fn propagate(&self, inputs: &[f32]) -> f32 {
+        assert_eq!(inputs.len(), self.weights.len());
+
+        let output = inputs
+            .iter()
+            .zip(&self.weights)
+            .map(|(input, weight)| input * weight)
+            .sum::<f32>();
+
+        (self.bias + output).max(0.0)
+    }
+}
