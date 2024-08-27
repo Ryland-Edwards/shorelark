47269edea10123b272b50a0e00bdf0814b106f1e
Ryland Edwards <rylandcedwards@gmail.com>
2024-08-26 19:26:33 -0500

    added tests in lib

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,3 +1,5 @@
+use rand::{Rng, RngCore};
+
 #[derive(Debug)]
 pub struct LayerTopology {
     pub neurons: usize,
@@ -10,10 +12,10 @@ pub struct Network {
 }
 
 impl Network {
-    pub fn random(layers: &[LayerTopology]) -> Self {
+    pub fn random(rng: &mut dyn RngCore, layers: &[LayerTopology]) -> Self {
         let layers = layers
             .windows(2)
-            .map(|layers| Layer::random(layers[0].neurons, layers[1].neurons))
+            .map(|layers| Layer::random(rng, layers[0].neurons, layers[1].neurons))
             .collect();
 
         Self {layers}
@@ -34,6 +36,14 @@ struct Layer {
 }
 
 impl Layer {
+    fn random(rng: &mut dyn RngCore, input_size: usize, output_size: usize) -> Self {
+        let neurons = (0..output_size)
+            .map(|_| Neuron::random(rng, input_size))
+            .collect();
+
+        Self { neurons }
+    }
+
     fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
         self.neurons
             .iter()
@@ -50,6 +60,17 @@ struct Neuron {
 }
 
 impl Neuron {
+    fn random(rng: &mut dyn RngCore, input_size: usize) -> Self {
+        let bias = rng.gen_range(-1.0..=1.0);
+
+        let weights = (0..input_size)
+            .map(|_| rng.gen_range(-1.0..=1.0))
+            .collect();
+
+        Self { bias, weights }
+    }
+
+
     fn propagate(&self, inputs: &[f32]) -> f32 {
         assert_eq!(inputs.len(), self.weights.len());
 
@@ -60,5 +81,48 @@ impl Neuron {
             .sum::<f32>();
 
         (self.bias + output).max(0.0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use approx::assert_relative_eq;
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha8Rng;
+
+    #[test]
+    fn random() {
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+        let neuron = Neuron::random(&mut rng, 4);
+
+        assert_relative_eq!(neuron.bias, -0.6255188);
+
+        assert_relative_eq!(neuron.weights.as_slice(),
+            [0.67383957, 0.8181262, 0.26284897, 0.5238807].as_ref()
+        );
+    }
+
+    #[test]
+    fn propagate() {
+        let neuron = Neuron {
+            bias: 0.5,
+            weights: vec![-0.3, 0.8],
+        };
+
+        // Ensures `.max()` (our ReLU) works:
+        assert_relative_eq!(
+        neuron.propagate(&[-10.0, -10.0]),
+        0.0,
+    );
+
+        // `0.5` and `1.0` chosen by a fair dice roll:
+        assert_relative_eq!(
+        neuron.propagate(&[0.5, 1.0]),
+        (-0.3 * 0.5) + (0.8 * 1.0) + 0.5,
+    );
+
+        // We could've written `1.15` right away, but showing the entire
+        // formula makes our intentions clearer
     }
 }
